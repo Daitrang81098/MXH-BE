018618c73dd02b77414206459e2602ebc90bf1a0
dai <1lanyeue@gmail.com>
2023-03-13 09:14:42 +0700

    sang de merge nhanh CR

---
+++ src/routes/AccountRouter.ts
@@ -0,0 +1,10 @@
+import {Router} from "express";
+import AccountController from "../controllers/AccountController";
+
+
+export const accountRouter = Router();
+accountRouter.post('/login', AccountController.login);
+accountRouter.get('/findById/:id', AccountController.findByIdUser);
+accountRouter.post('/register', AccountController.register);
+
+
--- src/services/AccountService.ts
+++ src/services/AccountService.ts
@@ -14,10 +14,41 @@ class AccountService {
     register = async (user) => {
         let userCheck = await this.accountRepository.findOneBy({username: user.username})
         if (!userCheck) {
-            user.userPassword = await bcrypt.hash(user.userPassword, 10);
+            user.userPassword = await bcrypt.hash(user.password, 10);
             return this.accountRepository.save(user);
         }
         return 'Username registered';
     }
-}
+    checkUser = async (user) => {
+        let userCheck = await this.accountRepository.findOneBy({username: user.username})
+        if (!userCheck) {
+            return 'User is not exit';
+        } else {
+            let passwordCompare = await bcrypt.compare(user.userPassword, userCheck.userPassword);
+            if (!passwordCompare) {
+                return 'Password is wrong'
+            } else {
+                let payload = {
+                    idUser: userCheck.idUser,
+                    username: userCheck.username,
+                }
+                return {
+                    idUser: userCheck.idUser,
+                    username: userCheck.username,
+                    token: jwt.sign(payload, SECRET, {
+                        expiresIn: 3000000
+                    })
+                }
+            }
+        }
 
+    }
+    findById = async (id)=> {
+        let account = await this.accountRepository.findOneBy({idUser:id});
+        if(!account){
+            return null;
+        }
+        return account;
+    }
+}
+export default AccountService
