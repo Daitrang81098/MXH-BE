a1c6565032740850dc4e7a4ac8d9e8064a133c49
dai <1lanyeue@gmail.com>
2023-03-19 15:59:54 +0700

    mer nhánh friend vào dev success

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -5,4 +5,5 @@ import {friendRouter} from "./FriendRouter";
 export const router = Router()
 router.use('/posts',postRouter);
 router.use("/accounts", accountRouter);
+
 router.use("/friends", friendRouter);
